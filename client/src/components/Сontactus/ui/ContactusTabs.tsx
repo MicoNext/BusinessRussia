c7a8ff8b4fe9017e6f9ac92f1a64251e7c04d5f8
eldarabdu <eldarabdu@gmail.com>
2025-10-17 14:44:09 +0300

    rework contactus section

--- client/src/components/Сontactus/ui/ContactusTabs.tsx
+++ client/src/components/Сontactus/ui/ContactusTabs.tsx
@@ -7,27 +7,25 @@ interface ContactusTabsProps {
 }
 
 export function ContactusTabs({ activeTab, setActiveTab }: ContactusTabsProps) {
+	const activeClasses =
+		'border-brand-primary/30 bg-brand-primary  cursor-not-allowed';
+	const normalClasses = clsx(
+		'bg-white/10 border-white/40',
+		'hover:bg-brand-accent/30 hover:border-brand-accent/60'
+	);
 	const buttonClassNames = {
 		general: clsx(
-			'flex-1 text-white border rounded-md p-2 transition-all duration-300',
-			'hover:bg-brand-accent/30 hover:border-brand-accent/60'
+			'flex-1 text-white border rounded-md p-2 transition-all duration-300'
 		),
-		form: clsx(
-			activeTab === 'form'
-				? 'bg-brand-accent/20 border-brand-accent/30'
-				: 'border-brand-primary/80 bg-brand-primary/30'
-		),
-		pay: clsx(
-			activeTab === 'pay'
-				? 'bg-brand-accent/20 border-brand-accent/60'
-				: 'border-brand-primary/80 bg-brand-primary/30'
-		),
+		form: clsx(activeTab === 'form' ? activeClasses : normalClasses),
+		pay: clsx(activeTab === 'pay' ? activeClasses : normalClasses),
 	};
 
 	return (
 		<div className='flex flex-wrap gap-4'>
 			<UnstyledButton
 				name='form'
+				disabled={activeTab === 'form'}
 				classNames={{
 					container: clsx(buttonClassNames.general, buttonClassNames.form),
 					content: clsx('items-start'),
@@ -38,6 +36,7 @@ export function ContactusTabs({ activeTab, setActiveTab }: ContactusTabsProps) {
 			</UnstyledButton>
 			<UnstyledButton
 				name='pay'
+				disabled={activeTab === 'pay'}
 				classNames={{
 					container: clsx(buttonClassNames.general, buttonClassNames.pay),
 					content: clsx('items-start'),
