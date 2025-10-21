9ca69f0087963a950616d0f740cb9e23a5b0568e
eldarabdu <eldarabdu@gmail.com>
2025-10-21 12:25:10 +0300

    added cross btn to modal

--- client/src/components/ContactModal/ui/ContactModal.tsx
+++ client/src/components/ContactModal/ui/ContactModal.tsx
@@ -1,7 +1,10 @@
 'use client';
 
 import { Form } from '@/components/Form';
+import { Button } from '@/components/ui/buttons';
+import { Headline } from '@/components/ui/Headline';
 import Modal from '@/components/ui/Modal';
+import { X } from 'lucide-react';
 
 export function ContactModal({ trigger }: { trigger: React.ReactNode }) {
 	return (
@@ -10,7 +13,28 @@ export function ContactModal({ trigger }: { trigger: React.ReactNode }) {
 
 			<Modal.Portal>
 				<Modal.Content className='w-full max-w-md p-6'>
-					<Form />
+					<Form
+						header={
+							<>
+								<Headline
+									title='Вступить'
+									order={4}
+									variant='dark'
+								/>
+								<Modal.Close asChild>
+									<Button
+										variant='ghost'
+										className='w-8 h-8 p-0 items-center justify-center'
+									>
+										<X
+											size={24}
+											strokeWidth={1}
+										/>
+									</Button>
+								</Modal.Close>
+							</>
+						}
+					/>
 				</Modal.Content>
 			</Modal.Portal>
 		</Modal>
--- client/src/components/ui/Modal/ui/Modal.tsx
+++ client/src/components/ui/Modal/ui/Modal.tsx
@@ -5,18 +5,20 @@ import { Trigger } from './Trigger';
 import { Content } from './Content';
 import { Portal } from '@/shared/lib/hooks';
 import type { IModalProps } from '../base';
+import { Close } from './Close';
 
 type ModalCompound = React.FC<IModalProps> & {
 	Trigger: typeof Trigger;
 	Content: typeof Content;
 	Portal: typeof Portal;
+	Close: typeof Close;
 };
 
 export const Modal: ModalCompound = Object.assign(Root, {
 	Trigger,
 	Content,
 	Portal,
+	Close,
 });
 
-export { Root, Trigger, Content, Portal };
 export default Modal;
